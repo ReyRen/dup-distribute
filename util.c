5e65d9c7e0d9ec796c6b59597533a63f7036a097
yuan ren <reyren179@gmail.com>
2022-11-07 14:05:14 +0800

    debug文件句柄close不掉的问题

--- util.c
+++ util.c
@@ -158,7 +158,7 @@ void master_receive(threadpool_t *thp) {
 
 	while(1) {
 		int res;
-        FILE *file;
+        FILE *file = {0x0};
 		bzero(&buf, sizeof(buf));
 		res = recv(master_acceptfd, &buf, sizeof(buf), (int)0);
 
@@ -175,7 +175,7 @@ void master_receive(threadpool_t *thp) {
                 16进制数，主要得知道接收的每个16进制数的大小。
                 char就是一个字节，unsigned char可以将打印出的16进的fff解决（是因为char是有符号的，16进制转换2进制头是1的话就会有fff）
             */
-            file = initDataRecord(&file, &uuid);
+            file = initDataRecord(file, &uuid);
             if (file == NULL) {
                 LogWrite(ERROR, "%d %s:", __LINE__, "master received message get failed");
                 return;
